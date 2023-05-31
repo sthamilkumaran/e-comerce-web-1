ad674a19963af5903a71267039bc5ab34f686383
sthamilkumaran <balapriyanka70@gmail.com>
2023-05-31 22:56:48 +0530

    dashboard start

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,12 +15,6 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
-
-    @livewireStyles
-
 </head>
 <body>
     <div id="app">
